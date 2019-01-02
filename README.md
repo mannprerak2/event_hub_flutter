e34d06741c2571aee91cf7a69566579d25060bde
Prerak Mann <mannprerak2@gmail.com>
2019-01-02 19:57:45 +0530

    Fetching posts from a page initially for testing api

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# events_flutter
+# Event Hub Flutter
 
-A flutter app to view events from facebook according to subscriptions
+### Base Idea
+```
+A facebook api based app for 
+    -finding events in institutes around you,
+    -following organisers and institutes for events, 
+```
+## Screens
 
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
+* Login screen----------------( with facebook only )
+* MainScreen----------------( 5 tabs at bottom, content area, top bar )
+    * Events Tab----------------( All events from following colleges first and then others )
+    * Subscription Tab----------------( Events from subscriptions only)
+    * Discover Tab----------------( Manage what you follow )
+    * Saved Tab----------------( Saved things ) 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +19,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Post {}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -27,24 +31,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String fbapi = "https://graph.facebook.com/v3.2/";
+  final List<String> pageIds = ["458226214198430"];
+
+  List<String> postIds = [];
+  List<String> picUrls = [];
+  FacebookLoginResult facebookLoginResult;
+  bool isLoggedIn = false;
+  int loginStatus = -1;
+
+  void onLoginStatusChanged(bool isLoggedIn, int loginStatus) {
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+      this.loginStatus = loginStatus;
+    });
+  }
+
+  void setPostIds(List<String> postIds) {
+    setState(() {
+      this.postIds = postIds;
+    });
+  }
+
+  void setPicUrls(List<String> picUrls) {
+    setState(() {
+      this.picUrls = picUrls;
+    });
+  }
+
+  void fetchPostIds() async {
+    List<String> postIds = [];
+    List<String> picUrls = [];
+    http.Response response = await http.get(fbapi +
+        "${pageIds[0]}" +
+        "/posts?fields=id"
+        "&access_token=${facebookLoginResult.accessToken.token}");
+    var dataObj = json.decode(response.body)["data"];
+    for (var obj in dataObj) {
+      postIds.add(obj["id"]);
+    }
+    setPostIds(postIds);
+    for (int i = 0; i < postIds.length; i++) {
+      http.Response response2 = await http.get(fbapi +
+          "${postIds[i]}" +
+          "?fields=full_picture" +
+          "&access_token=${facebookLoginResult.accessToken.token}");
+      var s = json.decode(response2.body)["full_picture"];
+      print(json.decode(response2.body));
+      if(s!=null)
+        picUrls.add(s);
+    }
+    print(picUrls);
+    setPicUrls(picUrls);
+  }
+
+  void loginFb() async {
+    var facebookLogin = FacebookLogin();
+    facebookLoginResult =
+        await facebookLogin.loginWithPublishPermissions(['manage_pages']);
+
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onLoginStatusChanged(false, 1);
+
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onLoginStatusChanged(false, 2);
+
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+        onLoginStatusChanged(true, 0);
+
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Center(
+      body:
+          Container(child: isLoggedIn ? buildAfterLogin() : buildBeforeLogin()),
+      floatingActionButton: isLoggedIn
+          ? FloatingActionButton(
+              onPressed: fetchPostIds,
+              child: Text("Fetch"),
+            )
+          : null,
+    );
+  }
 
-        child: Column(
+  Padding buildAfterLogin() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          child: ListView.builder(
+        itemCount: picUrls.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Image.network(picUrls[index]),
+          );
+        },
+      )),
+    );
+  }
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+  Center buildBeforeLogin() {
+    return Center(
+      child: RaisedButton(
+        onPressed: loginFb,
+        color: Colors.blue,
+        child: Text(
+          "Login",
+          style: TextStyle(color: Colors.white),
         ),
       ),
     );
