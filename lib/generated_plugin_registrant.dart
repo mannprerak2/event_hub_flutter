b2b192544ffe45dff27f500d67a30388da6ab930
Prerak Mann <mannprerak2@gmail.com>
2020-09-17 13:09:06 +0530

    added web setup

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,24 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  FirebaseFirestoreWeb.registerWith(registry.registrarFor(FirebaseFirestoreWeb));
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
+}
